b63a5208d5f6316d9c4adf43b4ba60c9fb251cd9
abishek17yadav <abishek17yadav@gmail.com>
2025-08-09 09:56:50 +0530

    Update CORS to allow deployed frontend

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // app.use(cors())
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://ai-frontend-ivory-nine.vercel.app', 
   credentials: true
 }));
 app.use(express.json())
